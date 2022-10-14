9c873fb30093917523a8b647b0314a05c39a0922
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-14 07:52:50 -0300

    feat: change prob1 implementation to be truly recursive

--- prob1.s
+++ prob1.s
@@ -11,29 +11,23 @@ jal x1, inverte
 beq x0, x0, FIM
 ##### START MODIFIQUE AQUI START #####
 inverte: 
-  addi sp, sp, -12 # allocate stack to receive 3 word values
-  sw x1, 8(sp) # stores function call return address at stack
-  sw x13 4(sp) # stores vector end address at stack
-  sw x12, 0(sp) #  stores vector begin address at stack
-  jal x1, elem_swap # calls elem_swap code block
+  addi x10, x12, 0 # attributes the start vector address to x10 register
+  addi sp, sp, -4 # allocate stack to receive 1 word
+  sw x1, 0(sp) # stores function call return address at stack
+  bge x13, x12, elem_swap # in the case that the end address of the considered vector is greater than the considered begin address, the elem_swap block is called
+  lw x1, 0(sp) # load function call return address at stack
+  addi sp, sp, 4 # deallocate stack to receive 1 word
+  jalr x0, 0(x1) # return to inverte call address
 
   elem_swap:  
-    blt x13, x12, quit_recursion # in the case that the end address of the considered vector is lower than the begin address, the quit_recursion block is called
     lw x28, 0(x12) # loads considered vector first value in x28
     lw x29, 0(x13) # loads considered vector last value in x29
     sw x28, 0(x13) # stores x28 value in the last position of the considered vector
     sw x29, 0(x12) # stores x29 value in the first position of the considered vector
     addi x12, x12, 4 # first element i from the considered vector passes to be i+1
     addi x13, x13, -4 # last element i from the considered vector passes to be i-1
-    jal x1, elem_swap # calls elem_swap code block recursively
+    jal x1, inverte # calls inverte function recursively
 
-  quit_recursion:
-    lw x12, 0(sp) # load initial vector begin address from stack
-    lw x13, 4(sp) # load initial vector end address from stack
-    lw x1, 8(sp) # load function call return address at stack
-    addi sp, sp, 12 # deallocate stack to receive 3 word values
-    addi x10, x12, 0 # attributes the start vector address to x10 register
-    jalr x0, 0(x1) # return to call address
 
 ##### END MODIFIQUE AQUI END #####
 FIM: add x1, x0, x10 # returns "inverte" x10 result value
