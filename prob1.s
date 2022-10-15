341e390963e286719bfcf7e2247dce5badc63bef
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-15 16:18:05 -0300

    fix: vector begin return at x10

--- prob1.s
+++ prob1.s
@@ -3,7 +3,7 @@ vetor: .word 1 2 3 4 5 6 7
 .text
 main:
 la x12, vetor # x12 = vector begin
-addi x13, x0, 7 # x13 = 7
+addi x13, x0, 7 # x13 = 7 = array length
 addi x13, x13, -1 # x13 = 6
 slli x13, x13, 2 # x13 = 24 = elements size
 add x13, x13, x12 # x13 = vector begin + elements size = vector size
@@ -11,8 +11,8 @@ jal x1, inverte
 beq x0, x0, FIM
 ##### START MODIFIQUE AQUI START #####
 inverte: 
-  addi x10, x12, 0 # attributes the start vector address to x10 register
-  addi sp, sp, -4 # allocate stack to receive 1 word
+  addi sp, sp, -8 # allocate stack to receive 1 word
+  sw x12, 4(sp) # stores current x12 value at stack
   sw x1, 0(sp) # stores function call return address at stack
   bge x13, x12, elem_swap # in the case that the end address of the considered vector is greater than the considered begin address, the elem_swap block is called
   beq x0, x0, exit_recursive # calls exit_recursive unconditionally to exit algorithm recursively
@@ -29,7 +29,9 @@ inverte:
 
   exit_recursive:
     lw x1, 0(sp) # load function call return address at stack
-    addi sp, sp, 4 # deallocate stack to receive 1 word
+    lw x12, 4(sp) # loads value at stack to x12 
+    addi sp, sp, 8 # deallocate stack to receive 1 word
+    addi x10, x12, 0 # attributes the start vector address to x10 register
     jalr x0, 0(x1) # return to inverte call address
 
 ##### END MODIFIQUE AQUI END #####
